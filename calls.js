4f484a963b0918902fc82a3d813917767ee1a915
Brayan Muñoz V <BrayanMnz@hotmail.com>
2023-01-19 09:46:12 -0400

    Deleting keys from call.js

--- calls.js
+++ calls.js
@@ -1,9 +1,9 @@
 // This will load the SDK and reuse your Fonos credentials
 const Fonos = require("@fonoster/sdk");
 const callManager = new Fonos.CallManager({
-  accessKeyId: "PJ619c4f2c9a495b0600000014",
+  accessKeyId: "nopushingkeys",
   accessKeySecret:
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJmb25vc3RlciIsInJvbGUiOiJQUk9KRUNUIiwiYWNjZXNzS2V5SWQiOiJQSjYxOWM0ZjJjOWE0OTViMDYwMDAwMDAxNCIsImlhdCI6MTYzNzYzMzgzNiwiZXhwIjoxNjY5MTkxNDM2fQ.InaOa41rxqX7U84pr5OyZKO5m0iKTWjlNNNuK8Y4ZqY",
+    "nopushingkeys",
 });
 
 // Few notes:
